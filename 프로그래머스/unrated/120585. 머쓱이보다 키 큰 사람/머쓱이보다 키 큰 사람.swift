9d172d6887267789758a7f9d04b2139ed06d7edd
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-10-04 17:09:51 +0900

    [unrated] Title: 머쓱이보다 키 큰 사람, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ array:[Int], _ height:Int) -> Int {
+    
+    var count: Int = 0
+    
+    for i in array {
+        if i > height {
+            count += 1
+        }
+    }
+    
+    return count
+}
