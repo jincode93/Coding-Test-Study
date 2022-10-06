7a207b2b49138da6426e738620bcb39472583d3d
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-10-06 10:32:05 +0900

    [unrated] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120905. n의 배수 고르기/n의 배수 고르기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ n:Int, _ numlist:[Int]) -> [Int] {
+    
+    var result: [Int] = []
+    
+    for i in numlist {
+        if i % n == 0 {
+            result += [i]
+        }
+    }
+    
+    return result
+}
