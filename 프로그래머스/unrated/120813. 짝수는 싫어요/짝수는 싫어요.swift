f96c21bf628564cfe057cb5fa2ab11b1be0d0e0b
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-10-06 16:36:02 +0900

    [unrated] Title: 짝수는 싫어요, Time: 0.08 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ n:Int) -> [Int] {
+    
+    var result: [Int] = []
+    
+    for i in 1...n {
+        if i % 2 == 1 {
+            result += [i]
+        }
+    }
+    
+    return result
+}
