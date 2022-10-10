4cc91794be31cde91452a191b8734ba722bf9a76
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-10-10 14:52:42 +0900

    [unrated] Title: 주사위의 개수, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ box:[Int], _ n:Int) -> Int {
+    
+    var result: Int = 1
+    
+    for i in box {
+        result *= (i / n)
+    }
+    
+    return result
+}
