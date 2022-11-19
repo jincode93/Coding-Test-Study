52c557cdafd4270d6ac07b4fc5a0be0b14b2113b
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-11-19 16:30:17 +0900

    [level 0] Title: 특이한 정렬, Time: 0.13 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ numlist:[Int], _ n:Int) -> [Int] {
+    
+    var numlistArray: [[Int]] = numlist.map{ [(n - $0) > 0 ? (n - $0) : (n - $0) * -1, $0] }
+    var result: [Int] = []
+    
+    numlistArray.sort(by: { $0[0] == $1[0] ? $0[1] > $1[1] : $0[0] < $1[0] })
+    
+    for i in numlistArray {
+        result += [i[1]]
+    }
+    
+    return result
+}
