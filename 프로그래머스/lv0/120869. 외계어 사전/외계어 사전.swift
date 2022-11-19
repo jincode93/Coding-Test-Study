094c31626758b7b458191774ab129a6e54c5c434
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-11-19 15:39:40 +0900

    [level 0] Title: 외계어 사전, Time: 0.12 ms, Memory: 16 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.swift
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.swift
@@ -1,23 +1,5 @@
 import Foundation
 
 func solution(_ spell:[String], _ dic:[String]) -> Int {
-    
-    var result: Int = 0
-    
-    for i in dic {
-        var a: [String] = i.map { String($0) }
-        var count: Int = 0
-        
-        for j in spell {
-            if a.contains(j) {
-                count += 1
-            }
-        }
-        
-        if spell.count == count {
-            result = 1
-        }
-    }
-    
-    return result == 1 ? 1 : 2
+    return dic.map { String( $0.sorted()) }.contains(spell.sorted().joined()) ? 1 : 2
 }
