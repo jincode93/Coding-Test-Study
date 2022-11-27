82a6885f99c330e1a7b41c3882494d068f06b69e
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-11-27 21:43:56 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.09 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.swift
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.swift
@@ -1,9 +1,3 @@
 func solution(_ seoul:[String]) -> String {
-    var result: String = ""
-    
-    if let a = seoul.firstIndex(of: "Kim") {
-        result = "김서방은 \(a)에 있다"
-    }
-    
-    return result
+    return "김서방은 \(seoul.index(of: "Kim")!)에 있다"
 }
