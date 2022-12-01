f34370492688f2a226664f1d3815e637e4d2f18d
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-12-01 23:06:55 +0900

    [level 1] Title: 음양 더하기, Time: 0.27 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    var result: [Int] = absolutes
+    
+    for i in 0..<signs.count {
+        if !signs[i] {
+            result[i] *= -1
+        }
+    }
+    
+    return result.reduce(0, +)
+}
