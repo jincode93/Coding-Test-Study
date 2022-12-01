9f1dec28ca6536385e3de88e07e1ec5e2f0b8130
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-12-01 23:53:42 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
@@ -1,6 +1,3 @@
 func solution(_ n:Int) -> String {
-    var num: Int = n
-    var str: String = String(repeating: "수박", count: num/2)
-    
-    return num % 2 == 0 ? str : str + "수"
+    return n % 2 == 0 ? String(repeating: "수박", count: n/2) : String(repeating: "수박", count: n/2) + "수"
 }
