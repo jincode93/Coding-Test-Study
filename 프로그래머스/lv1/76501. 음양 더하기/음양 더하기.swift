2721f36a6e3d1f50fab2281db55a2cbfef82565a
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-12-01 23:21:59 +0900

    [level 1] Title: 음양 더하기, Time: 0.74 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.swift
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.swift
@@ -1,13 +1,15 @@
 import Foundation
 
 func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
-    var result: [Int] = absolutes
+//     var result: [Int] = absolutes
     
-    for i in 0..<signs.count {
-        if !signs[i] {
-            result[i] *= -1
-        }
-    }
+//     for i in 0..<signs.count {
+//         if !signs[i] {
+//             result[i] *= -1
+//         }
+//     }
     
-    return result.reduce(0, +)
+//     return result.reduce(0, +)
+    
+    return (0..<absolutes.count).map { signs[$0] ? absolutes[$0] : -absolutes[$0] }.reduce(0, +)
 }
