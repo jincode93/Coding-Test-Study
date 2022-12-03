912dfc5adbc71fdab5bb48caee5c3f8637b3fbb8
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-12-03 18:15:42 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.24 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
@@ -1,20 +1,23 @@
 func solution(_ arr:[Int]) -> [Int] {
-    var min: Int = 100000
-    var index: Int = 0
-    var result: [Int] = arr
+//     var min: Int = 100000
+//     var index: Int = 0
+//     var result: [Int] = arr
     
-    if arr.count < 2 {
-        return [-1]
-    } else {
-        for i in 0..<arr.count {
-            if arr[i] < min {
-                min = arr[i]
-                index = i
-            }
-        }
+//     if arr.count < 2 {
+//         return [-1]
+//     } else {
+//         for i in 0..<arr.count {
+//             if arr[i] < min {
+//                 min = arr[i]
+//                 index = i
+//             }
+//         }
         
-        result.remove(at: index)
+//         result.remove(at: index)
         
-        return result
-    }
+//         return result
+//     }
+    let min: Int = arr.min() ?? 0
+    let resultArray: [Int] = arr.filter{ $0 != min }
+    return resultArray == [] ? [-1] : resultArray
 }
