fbdde0d189663d07dedf805587c921e693560cd5
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-12-03 18:46:46 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.swift
+++ 프로그래머스/lv1/12982. 예산/예산.swift
@@ -1,22 +1,42 @@
 import Foundation
 
 func solution(_ d:[Int], _ budget:Int) -> Int {
-    var sum: Int = 0
-    var count: Int = 0
-    let sortD: [Int] = d.sorted()
+//     var sum: Int = 0
+//     var count: Int = 0
+//     let sortD: [Int] = d.sorted()
     
-    if budget > d.reduce(0,+) {
-        return d.count
-    } else {
-        while sum < budget {
-            sum += sortD[count]
-            count += 1
-        }
+//     if budget > d.reduce(0,+) {
+//         return d.count
+//     } else {
+//         while sum < budget {
+//             sum += sortD[count]
+//             count += 1
+//         }
 
-        if sum > budget {
+//         if sum > budget {
+//             count -= 1
+//         }
+
+//         return count
+//     }
+    
+    var sortD: [Int] = d.sorted()
+    var count: Int = 0
+    var bud: Int = budget
+    
+    while sortD.count > 0 {
+        count += 1
+        if sortD.count != 1 {
+        bud -= sortD.removeFirst()
+        } else {
+            bud -= sortD[0]
+            sortD = []
+        }
+        if bud < 0 {
             count -= 1
+            break
         }
-
-        return count
     }
+    
+    return count
 }
