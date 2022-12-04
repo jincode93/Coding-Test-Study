8a309b765f27ace203b1f4aa65b3a1172be086b1
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-12-04 16:58:40 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.11 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
@@ -1,52 +1,10 @@
 import Foundation
 
 func solution(_ s:String) -> Int {
-    
-    var arr: [String] = s.map{ String($0) }
-    var result: [String] = []
-    
-    while arr.count > 0 {
-        if arr[0] == "z" {
-            arr.removeFirst(4)
-            result.append("0")
-        } else if arr[0] == "o" {
-            arr.removeFirst(3)
-            result.append("1")
-        } else if arr[0] == "t" {
-            if arr[1] == "w" {
-                arr.removeFirst(3)
-                result.append("2")
-            } else {
-                arr.removeFirst(5)
-                result.append("3")
-            }
-        } else if arr[0] == "f" {
-            if arr[1] == "o" {
-                arr.removeFirst(4)
-                result.append("4")
-            } else {
-                arr.removeFirst(4)
-                result.append("5")
-            }
-        } else if arr[0] == "s" {
-            if arr[1] == "i" {
-                arr.removeFirst(3)
-                result.append("6")
-            } else {
-                arr.removeFirst(5)
-                result.append("7")
-            }
-        } else if arr[0] == "e" {
-            arr.removeFirst(5)
-            result.append("8")
-        } else if arr[0] == "n" {
-            arr.removeFirst(4)
-            result.append("9")
-        } else {
-            result.append(arr[0])
-            arr.removeFirst()
-        }
+    let arr: [String] = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+    var str: String = s
+    for i in 0..<arr.count {
+        str = str.replacingOccurrences(of: arr[i], with: String(i))
     }
-    
-    return Int(result.joined(separator: ""))!
+    return Int(str)!
 }
