551fa235a134c9f0791d537e314ef2a658662310
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-12-06 22:25:44 +0900

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.swift
@@ -0,0 +1,45 @@
+// import Foundation
+
+// func solution(_ array:[Int], _ n:Int) -> Int {
+    
+//     var min: Int = 100
+//     var a: Int = 0
+//     var result: Int = 0
+    
+//     for i in array {
+//         a = abs(i - n)
+//         if min > a {
+//             min = a
+//             result = i
+//         } else if min == a {
+//             if result > i {
+//                 result = i
+//             }
+//         }
+//     }
+    
+//     return result
+// }
+
+import Foundation
+
+func solution(_ array:[Int], _ n:Int) -> Int {
+    
+    var min: Int = 100
+    var a: Int = 0
+    var result: Int = 0
+    
+    for i in array {
+        a = abs(i - n)
+        if min > a {
+            min = a
+            result = i
+        } else if min == a {
+            if result > i {
+                result = i
+            }
+        }
+    }
+    
+    return result
+}
