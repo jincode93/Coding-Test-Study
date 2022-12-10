2220625914aa587a244014ade80bde9970c68474
Junho Jin <111134273+jincode93@users.noreply.github.com>
2022-12-10 12:48:10 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ number:[Int]) -> Int {
+    var count: Int = 0
+    
+    for i in 0..<number.count-2 {
+        for j in i+1..<number.count-1 {
+            for k in j+1..<number.count {
+                if number[i] + number[j] + number[k] == 0 {
+                    count += 1
+                }
+            }
+        }
+    }
+    
+    return count
+}
